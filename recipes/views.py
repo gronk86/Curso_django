3870d677c5acd79014d9bd21e9412d02eb301dc4
Pedro Henrique <fonseca_ph@yahoo.com>
2024-09-16 20:32:08 -0300

    continuacao

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,6 @@ from django.shortcuts import render
 
 
 def home(request):
-   return render(request, 'recipes/home.html')
+   return render(request, 'recipes/pages/home.html')
 
 
-def contato(request):
-   return HttpResponse('contato')
-
-def sobre(request):
-   return HttpResponse('sobre')
