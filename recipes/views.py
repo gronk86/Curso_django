be0323d858bbd2de28bf1c1f8309e54402f85c43
Pedro Henrique <fonseca_ph@yahoo.com>
2024-09-21 23:59:24 -0300

    arquivos base

--- recipes/views.py
+++ recipes/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 def home(request):
    return render(request, 'recipes/pages/home.html')
 
+def recipe(request, id):
+   return render(request, 'recipes/pages/recipe-view.html')
 
