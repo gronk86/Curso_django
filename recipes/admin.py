ca95e6bba1672bea108dd3e15a6672a912ed7d0d
Pedro Henrique <fonseca_ph@yahoo.com>
2024-09-23 22:27:13 -0300

    admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category , Recipe
 
-# Register your models here.
+class CategoryaAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass 
+
+admin.site.register(Category ,CategoryaAdmin)
